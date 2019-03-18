0672ed03ba6104f02a105ce6a526d93337cc144c
liuyuqin <liuyuqin@jd.com>
2019-03-18 21:19:56 +0800

    react webpack typescript集成

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
             }
         ]
     },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
